780e6a65ade32d3ba772bb42152f033e31d366d6
Mels <mels.vagharshyandev@gmail.com>
2025-12-10 18:20:01 +0400

    fix

--- src/components/routine/utils/constants.ts
+++ src/components/routine/utils/constants.ts
@@ -33,8 +33,8 @@ export const specialistImages = [...specialists]
 export const clientResults = [
   {
     id: 1,
-    before: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762677423/IMG_5458_ouz7vr.jpg',
-    after: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762677423/IMG_5459_rmwtol.jpg',
+    before: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5458_ouz7vr.jpg',
+    after: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5459_rmwtol.jpg',
     title: 'Отбеливание зубов',
     description:
       'Проведена дентальная имплантация на нижней челюсти по протоколу «всё на 4х» имплантатах, фиксирована постоянная ортопедическая конструкция на нижнюю челюсть.',
@@ -43,8 +43,8 @@ export const clientResults = [
   },
   {
     id: 2,
-    before: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762581717/IMG_5402_ev87fp.jpg',
-    after: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762581717/IMG_5403_oqjd52.jpg',
+    before: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5402_ev87fp.jpg',
+    after: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5403_oqjd52.jpg',
     title: 'Протезирование',
     description:
       'Восстановлены разрушенные передние зубы мостовидным протезом. Улыбка и жевательная функция полностью восстановлены.',
@@ -53,8 +53,8 @@ export const clientResults = [
   },
   {
     id: 3,
-    before: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762582511/IMG_5409_vsmbzn.jpg',
-    after: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762582511/IMG_5407_krvmtp.jpg',
+    before: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5409_vsmbzn.jpg',
+    after: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5407_krvmtp.jpg',
     title: 'Реставрация зубов',
     description:
       'Выполнено восстановление сколотого переднего зуба с помощью композитной реставрации. Улыбка стала ровной и естественной.',
@@ -63,8 +63,8 @@ export const clientResults = [
   },
   {
     id: 4,
-    before: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762582681/IMG_5410_uq7fxo.jpg',
-    after: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762582681/IMG_5408_fj9kot.jpg',
+    before: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5410_uq7fxo.jpg',
+    after: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5408_fj9kot.jpg',
     title: 'Отбеливание зубов',
     description:
       'Удалены пятна и неровности эмали. Проведена композитная реставрация с восстановлением естественного блеска зубов.',
@@ -73,8 +73,8 @@ export const clientResults = [
   },
   {
     id: 5,
-    before: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762586873/IMG_5412_2_ieesoo.jpg',
-    after: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762586874/IMG_5406_2_w8j8ks.jpg',
+    before: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5412_2_ieesoo.jpg',
+    after: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5406_2_w8j8ks.jpg',
     title: 'Отбеливание зубов',
     description:
       'Восстановлена форма и цвет зубов. После реставрации улыбка стала более ровной и сияющей.',
@@ -83,8 +83,8 @@ export const clientResults = [
   },
   {
     id: 6,
-    before: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762587190/IMG_5416_ia9f7w.jpg',
-    after: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762587190/IMG_5415_vtuvor.jpg',
+    before: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5416_ia9f7w.jpg',
+    after: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5415_vtuvor.jpg',
     title: 'Отбеливание зубов',
     description:
       'Устранены сколы и потемнение эмали. Композитная реставрация вернула зубам природный цвет и гладкость.',
@@ -93,8 +93,8 @@ export const clientResults = [
   },
   {
     id: 7,
-    before: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762587322/IMG_5417_zpmf3a.jpg',
-    after: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762587322/IMG_5414_p8w9ez.jpg',
+    before: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5417_zpmf3a.jpg',
+    after: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5414_p8w9ez.jpg',
     title: 'Композитная реставрация',
     description:
       'Устранены дефекты передних зубов и выполнена композитная реставрация. Восстановлена ровность зубного ряда и естественный оттенок эмали.',
@@ -103,8 +103,8 @@ export const clientResults = [
   },
   {
     id: 8,
-    before: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762587454/IMG_5418_yphpwu.jpg',
-    after: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762587454/IMG_5413_pjdt5p.jpg',
+    before: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5418_yphpwu.jpg',
+    after: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5413_pjdt5p.jpg',
     title: 'Композитная реставрация',
     description:
       'Исправлены небольшие дефекты и неровности передних зубов. Композитная реставрация улучшила внешний вид улыбки и восстановила симметрию.',
@@ -113,8 +113,8 @@ export const clientResults = [
   },
   {
     id: 9,
-    before: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762582135/IMG_5404_hefnhw.jpg',
-    after: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762582135/IMG_5405_p8em2t.jpg',
+    before: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5404_hefnhw.jpg',
+    after: 'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_5405_p8em2t.jpg',
     title: 'Установка виниров',
     description:
       'Устранено сильное разрушение и потемнение зубов. Установлены керамические виниры E-max с естественным оттенком.',
