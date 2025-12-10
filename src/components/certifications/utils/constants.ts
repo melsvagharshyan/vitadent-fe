1be6eab1bd41796224ac15ce0c37544425501b3b
Mels <mels.vagharshyandev@gmail.com>
2025-12-10 19:14:05 +0400

    fix

--- src/components/certifications/utils/constants.ts
+++ src/components/certifications/utils/constants.ts
@@ -2,36 +2,36 @@ export const certifications = [
   {
     id: 1,
     image:
-      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762450748/IMG_20140916_113055_htgbp3.jpg',
+      'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_20140916_113055_htgbp3.jpg',
   },
   {
     id: 2,
     image:
-      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762450746/IMG_20140916_113012_hfzauu.jpg',
+      'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_20140916_113012_hfzauu.jpg',
   },
   {
     id: 3,
     image:
-      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762450746/IMG_20140916_113037_hiwhbh.jpg',
+      'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_20140916_113037_hiwhbh.jpg',
   },
 ]
 
 export const certificationsLarge = [
   {
     id: 1,
-    image: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762450730/2018.--003_d3gu9n.jpg',
+    image: 'https://melsimages.blob.core.windows.net/images/vitadent-images/2018.--003_d3gu9n.jpg',
   },
   {
     id: 2,
     image:
-      'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762450747/IMG_20140916_113231_cfhpnh.jpg',
+      'https://melsimages.blob.core.windows.net/images/vitadent-images/IMG_20140916_113231_cfhpnh.jpg',
   },
   {
     id: 3,
-    image: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762450729/2018.---001_kkqq3v.jpg',
+    image: 'https://melsimages.blob.core.windows.net/images/vitadent-images/2018.---001_kkqq3v.jpg',
   },
   {
     id: 4,
-    image: 'https://res.cloudinary.com/dxfqf6fgv/image/upload/v1762450730/2018.--002_tcon9s.jpg',
+    image: 'https://melsimages.blob.core.windows.net/images/vitadent-images/2018.--002_tcon9s.jpg',
   },
 ]
